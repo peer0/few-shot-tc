7fbce29c14696245be208910ef3c125670e18694
inistory <jungin3486@gmail.com>
2024-05-03 14:47:34 +0900

    [BUGFIX] log 저장

--- few-shot-tc/ensemble/code/run.sh
+++ few-shot-tc/ensemble/code/run.sh
@@ -6,7 +6,8 @@ for seed in 42; do
             for thres in 0.4; do
                 for lr in '5e-4' ; do
                     echo "python3 revised_main_ssl.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language" 
-                    python3 revised_main_ssl.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language 
+                    python3 revised_main_ssl.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language \
+                    > logs/codet5p_nshot${nshot}_thres${thres}_lr${lr}_seed${seed}_lang${language}_epoch20.log
                 done
 			done
 		done
