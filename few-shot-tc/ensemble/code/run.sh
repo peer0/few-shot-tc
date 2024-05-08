73f5253960a720df3176fae6ae0a7b6519e3409d
inistory <jungin3486@gmail.com>
2024-05-08 13:54:21 +0900

    [FEAT] forwhile aug 실험 - run.sh: 자연스러운 설정 - run2.sh: 인위적인 설정

--- few-shot-tc/ensemble/code/run.sh
+++ few-shot-tc/ensemble/code/run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 for seed in 42; do
-    for nshot in 5; do
-        for language in java; do
-            for thres in 0.4; do
-                for lr in '5e-4' ; do
+    for nshot in 5 10; do
+        for language in python; do
+            for thres in 0.7; do
+                for lr in '1e-4' '1e-5'; do
                     echo "python3 revised_main_ssl.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language" 
                     python3 revised_main_ssl.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language \
-                    > logs/codet5p_nshot${nshot}_thres${thres}_lr${lr}_seed${seed}_lang${language}_epoch20.log
+                    > logs/codet5p_nshot${nshot}_thres${thres}_lr${lr}_seed${seed}_lang${language}_epoch100.log 
                 done
 			done
 		done
 	done
 done
+5
---
+++ few-shot-tc/ensemble/code/run2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for seed in 42; do
+    for nshot in 5 10; do
+        for language in python; do
+            for thres in 0.7; do
+                for lr in '1e-4' '1e-5'; do
+                    echo "python3 revised_main_ssl2.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language" 
+                    python3 revised_main_ssl2.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language \
+                    > logs/codet5p_nshot${nshot}_thres${thres}_lr${lr}_seed${seed}_lang${language}_epoch100_v2.log 
+                done
+			done
+		done
+	done
+done
