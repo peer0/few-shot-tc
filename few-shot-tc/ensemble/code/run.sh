5c4e0ad6a7b358caa21da02234a8f91383253d9f
inistory <jungin3486@gmail.com>
2024-05-10 20:17:40 +0900

    [BUGFIX] 실행파일 업데이트 - codebert, unixcoder: v1,v2 실험을 위한 실행 파일 추가 - run.sh 에서는 결과를 command line에서 확인하기 위해 log 삭제 - codet5p.sh: 잘못 실험했던 lr 다시 실험하도록 추가

--- few-shot-tc/ensemble/code/run.sh
+++ few-shot-tc/ensemble/code/run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #test용
 for seed in 42; do
-    for nshot in 5 10; do
+    for nshot in 5; do
         for language in python; do
             for thres in 0.7; do
-                for lr in '1e-4' '1e-5'; do
+                for lr in '1e-4'; do
                     echo "python3 revised_main_ssl.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language" 
-                    python3 revised_main_ssl.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language \
-                    > logs/codet5p_nshot${nshot}_thres${thres}_lr${lr}_seed${seed}_lang${language}_epoch100.log 
+                    python3 revised_main_ssl2.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language
                 done
 			done
 		done
