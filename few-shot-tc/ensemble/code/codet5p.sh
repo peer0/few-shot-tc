6db73227be4d4e04cfa64271a4f8b5520d2c8105
inistory <jungin3486@gmail.com>
2024-05-17 17:27:28 +0900

    [BUGFIX] max epoch 50으로 변경

--- few-shot-tc/ensemble/code/codet5p.sh
+++ few-shot-tc/ensemble/code/codet5p.sh
@@ -1,25 +1,10 @@
-# #!/bin/bash
-
-# for seed in 42 43 45; do
-#     for nshot in 1 5 10; do
-#         for language in java python corcod; do
-#             for thres in 0.4 0.5 0.6; do
-#                 for lr in '1e-5' '1e-4' '5e-4' ; do
-#                     echo "python3 revised_main_ssl.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language" 
-#                     python3 revised_main_ssl.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language > result_log/${language}_${nshot}_codet5p_${lr}_${thres}_${seed}.log
-#                 done
-# 			done
-# 		done
-# 	done
-# done
-
 #!/bin/bash
 
 for seed in 42; do
-    for nshot in 5 10; do
+    for nshot in 20; do
         for language in python; do
             for thres in 0.7; do
-                for lr in '4e-05' '5e-04'; do
+                for lr in '4e-05' '1e-4'; do
                     echo "python3 revised_main_ssl.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language" 
                     python3 revised_main_ssl.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language \
                     > logs/codet5p/v1/codet5p_nshot${nshot}_thres${thres}_lr${lr}_seed${seed}_lang${language}_epoch50.log 
--- few-shot-tc/ensemble/code/codet5p2.sh
+++ few-shot-tc/ensemble/code/codet5p2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 for seed in 42; do
-    for nshot in 5 10; do
+    for nshot in 20; do
         for language in python; do
             for thres in 0.7; do
-                for lr in '1e-5' '4e-05' '1e-4' '5e-4'; do
+                for lr in '4e-05' '1e-4'; do
                     echo "python3 revised_main_ssl2.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language" 
                     python3 revised_main_ssl2.py --config configs/codet5p.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language \
                     > logs/codet5p/v2/codet5p_nshot${nshot}_thres${thres}_lr${lr}_seed${seed}_lang${language}_epoch50_v2.log 
@@ -13,5 +13,3 @@ for seed in 42; do
 		done
 	done
 done
-
-#!/bin/bash
