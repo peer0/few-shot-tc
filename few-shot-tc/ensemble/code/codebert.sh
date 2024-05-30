bbab8e6819a8903afea089291919573eb98114c4
inistory <jungin3486@gmail.com>
2024-05-30 17:25:57 +0900

    [REFACTOR] 시간기록, model, aug(forwhile,backtranslation), version(v1/v2) 추가

--- few-shot-tc/ensemble/code/codebert.sh
+++ few-shot-tc/ensemble/code/codebert.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+echo "### START DATE=$(date)" 
+echo "### HOSTNAME=$(hostname)" 
+echo "### CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES" 
+
+
 for seed in 43; do
-    for nshot in 5 10 20; do
+    for nshot in 5 10; do
         for language in python; do
             for thres in 0.7; do
-                for lr in '2e-4'; do
-                    echo "python3 revised_main_ssl.py --config configs/codebert.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language" 
-                    python3 revised_main_ssl.py --config configs/codebert.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language \
-                    > logs/codebert/${seed}/v1/${language}_${nshot}_codebert_${lr}_${thres}_${seed}_epoch50.log
+                for model in 'codebert'; do
+                    for lr in '1e-04' '4e-05'; do
+                        for aug in 'forwhile' 'back-translation'; do
+                            for version in 'v1'; do
+                                log_path="logs/${model}/${seed}/${aug}/${version}/${model}_nshot${nshot}_thres${thres}_lr${lr}_seed${seed}_lang${language}_epoch50_${version}.log"
+                                log_dir=$(dirname ${log_path})
+                                mkdir -p ${log_dir}
+                                echo "python revised_main_ssl.py --config configs/${model}.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language" 
+                                python revised_main_ssl.py --config configs/${model}.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language --aug $aug --version $version\
+                                > ${log_path}
+                            done
+                        done
+                    done
                 done
-			done
-		done
-	done
+            done
+        done
+    done
 done
+
+echo "###" 
+echo "### END DATE=$(date)"
--- few-shot-tc/ensemble/code/graphcodebert.sh
+++ few-shot-tc/ensemble/code/graphcodebert.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
+echo "### START DATE=$(date)" 
+echo "### HOSTNAME=$(hostname)" 
+echo "### CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES" 
 
 for seed in 43; do
     for nshot in 5 10; do
         for language in python; do
             for thres in 0.7; do
-                for lr in '2.5e-04' '2e-04'; do
-                    echo "python3 revised_main_ssl.py --config configs/graphcodebert.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language" 
-                    python3 revised_main_ssl.py --config configs/graphcodebert.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language \
-                    > logs/graphcodebert/${seed}/v1/graphcodebert_nshot${nshot}_thres${thres}_lr${lr}_seed${seed}_lang${language}_epoch50.log 
+                for model in 'graphcodebert'; do
+                    for lr in '2.5e-04' '2e-04'; do
+                        for aug in 'forwhile' 'back-translation'; do
+                            for version in 'v1'; do
+                                log_path="logs/${model}/${seed}/${aug}/${version}/${model}_nshot${nshot}_thres${thres}_lr${lr}_seed${seed}_lang${language}_epoch50_${version}.log"
+                                log_dir=$(dirname ${log_path})
+                                mkdir -p ${log_dir}
+                                echo "python revised_main_ssl.py --config configs/${model}.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language" 
+                                python revised_main_ssl.py --config configs/${model}.json --n_labeled_per_class $nshot --psl_threshold_h $thres --lr $lr --seed $seed --dataset $language --aug $aug --version $version\
+                                > ${log_path}
+                            done
+                        done
+                    done
                 done
-			done
-		done
-	done
+            done
+        done
+    done
 done
+echo "###" 
+echo "### END DATE=$(date)"
+
