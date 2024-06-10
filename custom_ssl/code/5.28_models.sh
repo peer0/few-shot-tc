9600386401b85961eb6ab1ef3ef792fb70b01b75
imsuhan22 <imsuhan22@node03.toc.lab>
2024-06-10 14:55:25 +0900

    joint match change lr, baseline

--- custom_ssl/code/5.28_models.sh
+++ custom_ssl/code/5.28_models.sh
@@ -1,28 +1,31 @@
-for nshot in 5; do
-    for language in corcod ; do
-        for pse_cl in 5 ; do
-            for thres in  0.7; do
-                for max_epoch in 37; do             
-                    #for model in  'codesage' 'codebert' 'codet5p' 'graphcodebert' 'unixcoder' ; do
-                    for model in 'codebert' 'codet5p' 'graphcodebert' 'unixcoder' ; do
-					    for seed in 42 43 45; do   
-							if [ "${model}" = "codesage" ]; then
-								lr_values='4e-4'  # Only two learning rates for codesage
-							elif [ "${model}" = "codebert" ]; then
-								lr_values='4e-4'
-							elif [ "${model}" = "codet5p" ]; then
-								lr_values='3e-4'
-							elif [ "${model}" = "graphcodebert" ]; then
-								lr_values='2.5e-4'
-							elif [ "${model}" = "unixcoder" ]; then
-								lr_values='4e-4'
-							fi
+for nshot in 5 ; do
+    for language in java python corcod ; do
+        if [ "${language}" = "corcod" ]; then
+            pse_cl=5
+        else
+            pse_cl=7
+        fi
+        for thres in 0.7; do
+            for max_epoch in 20; do
+                for model in 'codebert' 'codet5p' 'graphcodebert' 'unixcoder' ; do
+                    #for seed in 42 43 45; do
+                    for seed in 42; do
+                        if [ "${model}" = "codesage" ]; then
+                            lr_values='2e-5'  # Only two learning rates for codesage
+                        elif [ "${model}" = "codebert" ]; then
+                            lr_values='2e-5'
+                        elif [ "${model}" = "codet5p" ]; then
+                            lr_values='2e-5'
+                        elif [ "${model}" = "graphcodebert" ]; then
+                            lr_values='2e-5'
+                        elif [ "${model}" = "unixcoder" ]; then
+                            lr_values='1e-5'
+                        fi
 
-                        	for lr in $lr_values ; do
-                            	echo "python3 panel_main_load.py ${nshot} ${model} ${lr} ${thres} ${max_epoch} ../data/problem_based_split/${language}_extended_data  ${pse_cl} > ../result_log_balance/${language}/${model}/class${pse_cl}/${nshot}_${model}_${lr}_${thres}_${language}_${pse_cl}_${max_epoch}_${seed}.log" 
-                                python3 panel_main_load.py ${nshot} ${model} ${lr} ${thres} ${max_epoch} ../data/problem_based_split/${language}_extended_data ${pse_cl} ${seed}> ../result_log_balance/${language}/${model}/class${pse_cl}/${nshot}_${model}_${lr}_${thres}_${language}_${pse_cl}_${max_epoch}_${seed}.log         
-							done
-						done
+                        for lr in $lr_values ; do
+                            echo "python3 panel_main_load.py ${nshot} ${model} ${lr} ${thres} ${max_epoch} ../data/problem_based_split/${language}_extended_data  ${pse_cl} > ../result_log_balance/${language}/${model}/class${pse_cl}/${nshot}_${model}_${lr}_${thres}_${language}_${pse_cl}_${max_epoch}_${seed}.log" 
+                            python3 panel_main_load.py ${nshot} ${model} ${lr} ${thres} ${max_epoch} ../data/problem_based_split/${language}_extended_data ${pse_cl} ${seed}> ../result_log_balance/${language}/${model}/class${pse_cl}/${nshot}_${model}_${lr}_${thres}_${language}_${pse_cl}_${max_epoch}_${seed}.log         
+                        done
                     done
                 done
             done
--- custom_ssl/code/5.28_models10.sh
+++ custom_ssl/code/5.28_models10.sh
@@ -1,28 +1,30 @@
 for nshot in 10; do
-    for language in corcod ; do
-        #for pse_cl in 7 ; do
-        for pse_cl in 5 ; do
-            for thres in  0.7; do
-                for max_epoch in 37; do             
-                    #for model in  'codesage' 'codebert' 'codet5p' 'graphcodebert' 'unixcoder' ; do
-                    for model in 'codebert' 'codet5p' 'graphcodebert' 'unixcoder' ; do
-                        for seed in 42 43 45; do
-                            if [ "${model}" = "codesage" ]; then
-                                lr_values='3e-4'  # Two learning rates for codesage
-                            elif [ "${model}" = "codebert" ]; then
-                                lr_values='2e-4'  # One learning rate for codebert
-                            elif [ "${model}" = "codet5p" ]; then
-                                lr_values='2.5e-4'  # One learning rate for codet5p
-                            elif [ "${model}" = "graphcodebert" ]; then
-                                lr_values='2e-4'  # One learning rate for graphcodebert
-                            elif [ "${model}" = "unixcoder" ]; then
-                                lr_values='2e-4'  # One learning rate for unixcoder
-                            fi
+    for language in java python corcod ; do
+        if [ "${language}" = "corcod" ]; then
+            pse_cl=5
+        else
+            pse_cl=7
+        fi
+        for thres in 0.7; do
+            for max_epoch in 37; do             
+                for model in 'codebert' 'codet5p' 'graphcodebert' 'unixcoder' ; do
+                    #for seed in 42 43 45; do
+                    for seed in 42; do
+                        if [ "${model}" = "codesage" ]; then
+                            lr_values='3e-5'  # Two learning rates for codesage
+                        elif [ "${model}" = "codebert" ]; then
+                            lr_values='3e-5'  # One learning rate for codebert
+                        elif [ "${model}" = "codet5p" ]; then
+                            lr_values='3e-5'  # One learning rate for codet5p
+                        elif [ "${model}" = "graphcodebert" ]; then
+                            lr_values='3e-5'  # One learning rate for graphcodebert
+                        elif [ "${model}" = "unixcoder" ]; then
+                            lr_values='2e-6'  # One learning rate for unixcoder
+                        fi
 
-                            for lr in $lr_values ; do
-                                echo "python3 panel_main_load.py ${nshot} ${model} ${lr} ${thres} ${max_epoch} ../data/problem_based_split/${language}_extended_data  ${pse_cl} > ../result_log_balance/${language}/${model}/class${pse_cl}/${nshot}_${model}_${lr}_${thres}_${language}_${pse_cl}_${max_epoch}_${seed}.log" 
-                                python3 panel_main_load.py ${nshot} ${model} ${lr} ${thres} ${max_epoch} ../data/problem_based_split/${language}_extended_data ${pse_cl} ${seed}> ../result_log_balance/${language}/${model}/class${pse_cl}/${nshot}_${model}_${lr}_${thres}_${language}_${pse_cl}_${max_epoch}_${seed}.log         
-                            done
+                        for lr in $lr_values ; do
+                            echo "python3 panel_main_load.py ${nshot} ${model} ${lr} ${thres} ${max_epoch} ../data/problem_based_split/${language}_extended_data  ${pse_cl} > ../result_log_balance/${language}/${model}/class${pse_cl}/${nshot}_${model}_${lr}_${thres}_${language}_${pse_cl}_${max_epoch}_${seed}.log" 
+                            python3 panel_main_load.py ${nshot} ${model} ${lr} ${thres} ${max_epoch} ../data/problem_based_split/${language}_extended_data ${pse_cl} ${seed}> ../result_log_balance/${language}/${model}/class${pse_cl}/${nshot}_${model}_${lr}_${thres}_${language}_${pse_cl}_${max_epoch}_${seed}.log         
                         done
                     done
                 done
